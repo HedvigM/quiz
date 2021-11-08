753b11875cd8dedbcdc3c237fc034bd50fa149cf
HedvigM <hedvig@mejstedt.se>
2021-11-08 19:15:08 +0100

    codealong on monday's lecture
    also adding some new functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import { quiz } from 'reducers/quiz'
+import React from 'react';
+import { Provider } from 'react-redux';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { quiz } from 'reducers/quiz';
 
-import { CurrentQuestion } from 'components/CurrentQuestion'
+import { CurrentQuestion } from 'components/CurrentQuestion';
+import { LastPage } from 'components/LastPage';
 
+/* preparing the app for the store */
 const reducer = combineReducers({
-  quiz: quiz.reducer
-})
+  quiz: quiz.reducer,
+});
 
-const store = configureStore({ reducer })
+const store = configureStore({ reducer });
+console.log(store.getState().quiz);
 
+/* provider is imported from the redux package. Serves the same purpose as browserRouter. We have to wrapp the whole app in this to be able to reach the store. */
 export const App = () => {
   return (
     <Provider store={store}>
+      <h1>kuken{store.getState().quiz.quizOver.toString()}</h1>
+      {store.getState().quiz.quizOver === true && <LastPage />}
       <CurrentQuestion />
     </Provider>
-  )
-}
+  );
+};
--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,16 +1,52 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { quiz } from '../reducers/quiz';
+import styled from 'styled-components';
 
 export const CurrentQuestion = () => {
-  const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
+  const question = useSelector(
+    (state) => state.quiz.questions[state.quiz.currentQuestionIndex]
+    /* man kan välja om man vill att det skall heta store eller state...  */
+  );
+
+  /*  const quizSlice = useSelector((store) => store.quiz); */
+  /* console.log(quizSlice);
+  console.log(question); */
+
+  const dispatch = useDispatch();
+  /* dispatch some actions */
 
   if (!question) {
-    return <h1>Oh no! I could not find the current question!</h1>
+    return <h1>Oh no! I could not find the current question!</h1>;
   }
 
+  const onAnswerSubmit = (id, index) => {
+    dispatch(
+      quiz.actions.submitAnswer({
+        questionId: id,
+        answerIndex: index,
+      })
+    );
+    dispatch(quiz.actions.goToNextQuestion());
+  };
+
   return (
-    <div>
+    <Question>
       <h1>Question: {question.questionText}</h1>
-    </div>
-  )
-}
+      {question.options.map((item, index) => (
+        <button
+          type='button'
+          key={item}
+          onClick={() => onAnswerSubmit(question.id, index)}
+        >
+          {item}
+        </button>
+      ))}
+    </Question>
+  );
+};
+
+const Question = styled.div`
+  height: 100vh;
+  background-color: grey;
+`;
